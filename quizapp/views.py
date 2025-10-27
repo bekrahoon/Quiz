80179bb811ea1b25e2601a29814343781f39a82e
Bekrahun <bekrahoon12@gmail.com>
2025-10-27 13:16:58 +0600

    изменено:      quizapp/views.py

--- quizapp/views.py
+++ quizapp/views.py
@@ -66,50 +66,7 @@ class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
 
-# Read: Список вопросов
-class QuestionListView(View):
-    def get(self, request):
-        questions = Question.objects.all()
-        return render(request, 'question_list.html', {'questions': questions})
 
-# Create: Добавление вопроса
-class QuestionCreateView(View):
-    def get(self, request):
-        form = QuestionForm()
-        return render(request, 'question_form.html', {'form': form})
-    
-    def post(self, request):
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('question_list')
-        return render(request, 'question_form.html', {'form': form})
-
-# Update: Редактирование вопроса
-class QuestionUpdateView(View):
-    def get(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        form = QuestionForm(instance=question)
-        return render(request, 'question_form.html', {'form': form})
-    
-    def post(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        form = QuestionForm(request.POST, instance=question)
-        if form.is_valid():
-            form.save()
-            return redirect('question_list')
-        return render(request, 'question_form.html', {'form': form})
-
-# Delete: Удаление вопроса
-class QuestionDeleteView(View):
-    def get(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        return render(request, 'question_confirm_delete.html', {'question': question})
-    
-    def post(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        question.delete()
-        return redirect('question_list')
     
 # Quiz: Главная страница игры
 class QuizView(View):
